675bfd754b07bb96e06ea6e4caa612295444bb15
GregoriSimei <40555037+GregoriSimei@users.noreply.github.com>
2020-06-10 22:07:12 -0300

    Created a general DbContext file

--- server/Context/DataContext.cs
+++ server/Context/DataContext.cs
@@ -7,16 +7,23 @@ using server.Classes;
 
 namespace server.Context
 {
-    public class UserDbContext : DbContext
+    public class DataContext : DbContext
     {
-
         public DbSet<User> users { get; set; }
-
+        public DbSet<Business> business { get; set; }
+        public DbSet<Coupon> coupons { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<CouponClient> couponClients { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CouponClient>().HasKey("couponID", "userID");
+        }
+
     }
 }
-
