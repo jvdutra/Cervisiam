dcfcf659740d4a45de2935daa174d6b8441b03e9
João Gonçalves <joaovitordutrag@gmail.com>
2020-06-24 13:28:21 -0300

    Added coupons page

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,19 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Button, Row, Col, Badge, Modal } from 'react-bootstrap';
 import { Map, Marker, TileLayer } from 'react-leaflet';
 
 import Layout from '../../components/Layout';
+import api from '../../services/api';
+
 import businessPicture from '../../assets/business.jpg';
 
 import './styles.css';
 
 interface Coupon {
     id: number,
-    coupon: {
-        id: number,
-        type: string,
-        value: number
-    },
+    type: string,
+    value: number
     business: {
         id: number,
         name: string,
@@ -24,96 +23,37 @@ interface Coupon {
     }
 }
 
-const coupons: Coupon[] = [
-    {
-        id: 5,
-        coupon: {
-            id: 1,
-            type: 'DISCOUNT',
-            value: 10
-        },
-        business: {
-            id: 1,
-            name: 'Bar do Dilson',
-            uf: 'PR',
-            city: 'Curitiba',
-            latitude: -25.431881,
-            longitude: -49.278231
-        }
-    },
-    {
-        id: 4,
-        coupon: {
-            id: 1,
-            type: 'DISCOUNT',
-            value: 10
-        },
-        business: {
-            id: 1,
-            name: 'Bar do Dilson',
-            uf: 'PR',
-            city: 'Curitiba',
-            latitude: -25.433121,
-            longitude: -49.283702
-        }
-    },
-    {
-        id: 3,
-        coupon: {
-            id: 1,
-            type: 'PERCENTAGE',
-            value: 10
-        },
-        business: {
-            id: 1,
-            name: 'Bar do Dilson',
-            uf: 'PR',
-            city: 'Curitiba',
-            latitude: -25.436066,
-            longitude: -49.283981
-        }
-    }
-]
-
-const myCoupons: Coupon[] = [
-    {
-        id: 1,
-        coupon: {
-            id: 1,
-            type: 'PERCENTAGE',
-            value: 10
-        },
-        business: {
-            id: 1,
-            name: 'Bar roxo',
-            uf: 'PR',
-            city: 'Curitiba',
-            latitude: -25.433082,
-            longitude: -49.280055
-        }
-    },
-    {
-        id: 2,
-        coupon: {
-            id: 1,
-            type: 'DISCOUNT',
-            value: 10
-        },
-        business: {
-            id: 1,
-            name: 'Da Dilsa',
-            uf: 'PR',
-            city: 'Curitiba',
-            latitude: -25.436008,
-            longitude: -49.279411
-        }
-    }
-]
-
 const Dashboard = () => {
     const [infoModalShow, setInfoModalShow] = useState(false);
-
     const [selectedCoupon, setSelectedCoupon] = useState<Coupon>();
+    const [coupons, setCoupons] = useState<Coupon[]>();
+
+    useEffect(() => {
+        api.get(`/coupons`).then(async res => {
+            const couponsResponse = res.data;
+            const couponsList: Coupon[] = [];
+
+            await couponsResponse.map((coupon: Coupon) => {
+                const parsedCoupon = {
+                    id: coupon.id,
+                    type: coupon.type,
+                    value: coupon.value,
+                    business: {
+                        id: coupon.business.id,
+                        name: coupon.business.name,
+                        uf: coupon.business.uf,
+                        city: coupon.business.city,
+                        latitude: coupon.business.latitude,
+                        longitude: coupon.business.longitude
+                    }
+                }
+
+                return couponsList.push(parsedCoupon);
+            });
+
+            setCoupons(couponsList);
+        });
+    }, []);
 
     function handleViewCouponInformation(coupon: Coupon) {
         setSelectedCoupon(coupon);
@@ -150,57 +90,28 @@ const Dashboard = () => {
                         <p>
                             <strong>Cupom:</strong><br></br>
                             { 
-                                selectedCoupon?.coupon.type === 'DISCOUNT' ?
-                                `R$ ${selectedCoupon?.coupon.value}` :
-                                `${selectedCoupon?.coupon.value}% OFF`
+                                selectedCoupon?.type === 'DISCOUNT' ?
+                                `R$ ${selectedCoupon?.value}` :
+                                `${selectedCoupon?.value}% OFF`
                             }
                         </p>
                     </Modal.Body>
                 </Modal>
 
-                <h1>Seus cupons</h1>
-                <div className="separator mb-3"></div>
-                <div className="coupons-content mb-3">
-                    <Row>
-                        {myCoupons.map((myCoupon: Coupon) => (
-                            <Col md="4" key={myCoupon.id}>
-                                <Card style={{ width: '18rem' }}>
-                                    <Card.Img variant="top" src={businessPicture} />
-                                    <Card.Body>
-                                        <Badge variant="danger" className="mb-3" style={{ fontSize: 18 }}>
-                                            {
-                                                myCoupon.coupon.type === 'DISCOUNT' ?
-                                                `R$ ${myCoupon.coupon.value}` :
-                                                `${myCoupon.coupon.value}% OFF`
-                                            }
-                                        </Badge>
-                                        <Card.Title>
-                                            { myCoupon.business.name } 
-                                        </Card.Title>
-                                        <Card.Text>
-                                            Em { `${myCoupon.business.city}/${myCoupon.business.uf}` }
-                                        </Card.Text>
-                                        <Button variant="warning" onClick={() => handleViewCouponInformation(myCoupon)}>Ver</Button>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        ))}
-                    </Row>
-                </div>
                 <h1>Cupons disponíveis</h1>
                 <div className="separator mb-3"></div>
                 <div className="coupons-content mb-3">
                     <Row>
-                        {coupons.map((coupon: Coupon) => (
-                            <Col md="4" key={coupon.id}>
+                        {coupons?.map((coupon: Coupon) => (
+                            <Col md="4" className="mb-5" key={coupon.id}>
                                 <Card style={{ width: '18rem' }}>
                                     <Card.Img variant="top" src={businessPicture} />
                                     <Card.Body>
                                         <Badge variant="danger" className="mb-3" style={{ fontSize: 18 }}>
                                             {
-                                                coupon.coupon.type === 'DISCOUNT' ?
-                                                `R$ ${coupon.coupon.value}` :
-                                                `${coupon.coupon.value}% OFF`
+                                                coupon.type === 'DISCOUNT' ?
+                                                `R$ ${coupon.value}` :
+                                                `${coupon.value}% OFF`
                                             }
                                         </Badge>
                                         <Card.Title>
@@ -209,7 +120,6 @@ const Dashboard = () => {
                                         <Card.Text>
                                             Em { `${coupon.business.city}/${coupon.business.uf}` }
                                         </Card.Text>
-                                        <Button variant="warning" className="mr-1">Pegar cupom</Button>
                                         <Button variant="warning" onClick={() => handleViewCouponInformation(coupon)}>Ver</Button>
                                     </Card.Body>
                                 </Card>
